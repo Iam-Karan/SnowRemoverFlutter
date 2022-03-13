45ecc6c15d32c08dd5cc22426e8dd569557fddd6
Karan Patel <75926283+Iam-Karan@users.noreply.github.com>
2022-03-13 10:34:10 -0400

    add color theme in constant.dart file

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color PrimaryColor = Color(0xFF34A8DB);
+const Color SecondaryColor = Colors.white;
+const Color PinkColor = Color(0xFFF14E61);
+const Color DarkBlueColor = Color(0xFF3463DB);
