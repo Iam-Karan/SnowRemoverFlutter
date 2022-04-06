969c2d7e1213a977b8ee8c16571391cadca7bb72
simarmultani <Simarjitsingh32@gmail.com>
2022-04-06 05:44:47 -0400

    product details form + validation extension

---
+++ lib/validaiton_extension.dart
@@ -0,0 +1,36 @@
+extension ExtString on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidName {
+    final nameRegExp =
+        RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidPassword {
+    final passwordRegExp = RegExp(
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\><*~]).{8,}/pre>');
+    return passwordRegExp.hasMatch(this);
+  }
+
+  bool get isStringEmpty {
+    return this != null;
+  }
+
+  bool get isValidPhone {
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(this);
+  }
+
+  bool get numbersOnly {
+    final RegExp numberRegExp = RegExp(r'\d');
+    return numberRegExp.hasMatch(this);
+  }
+
+  bool get validURL {
+    return Uri.parse(this).isAbsolute;
+  }
+}
